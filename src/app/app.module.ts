eb80cc6ee00413d9f90139624c061b6493cd0aac
kimjunghwan <writer0713@naver.com>
2018-06-26 19:53:49 +0900

    app.module.ts 에 필요 컴포넌트, 서비스 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { TodoService } from './service/todo.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './component/todo-list/todo-list.component';
+import { TodoItemComponent } from './component/todo-item/todo-item.component';
+import { InputComponent } from './component/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    TodoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
