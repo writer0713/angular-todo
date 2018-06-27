1fda4f6666e713a36e0f5fff473c279c64f4f8bb
kimjunghwan <writer0713@naver.com>
2018-06-27 12:08:07 +0900

    service test 추가

--- src/app/component/input/input.component.spec.ts
+++ src/app/component/input/input.component.spec.ts
@@ -1,14 +1,18 @@
+import { TodoService } from './../../service/todo.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputComponent } from './input.component';
+import { Todo } from '../../model/todo';
 
 describe('InputComponent', () => {
   let component: InputComponent;
   let fixture: ComponentFixture<InputComponent>;
+  let todoService: TodoService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InputComponent ]
+      declarations: [ InputComponent ],
+      providers: [TodoService]
     })
     .compileComponents();
   }));
@@ -16,10 +20,19 @@ describe('InputComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(InputComponent);
     component = fixture.componentInstance;
+    todoService = TestBed.get(TodoService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('can add todo', () => {
+    spyOn(todoService, 'getTodoList').and.returnValues([new Todo('1'), new Todo('2')]);
+    expect(todoService.getTodoList().length).toEqual(2);
+    todoService.add(new Todo('3'));
+    expect(todoService.getTodoList().length).toEqual(3);
+
+  })
 });
